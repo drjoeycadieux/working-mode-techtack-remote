28a194c856390c20833a4f1976d3f052cd4b9bd2
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-07-01 09:28:13 +0000

    Update 5 files.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,6 +3,7 @@
 <template>
   <div class="navbar-menu">
     <ul>
+      <p class="title">Techtack Technologies (remote)</p>
       <router-link class="linknav" to="/">Home</router-link>
       <router-link class="linknav" to="/">Global Network</router-link>
     </ul>
@@ -14,6 +15,13 @@
   padding: 25px;
   background-color: rgb(20, 26, 80);
 }
+.title {
+  display: inline;
+  margin: 8px;
+  font-family: sans-serif;
+  color: white;
+  font-size: 26px;
+}
 .navbar-menu ul {
   display: block;
 }
@@ -22,5 +30,8 @@
   text-decoration: none;
   font-family: sans-serif;
   margin: 8px;
+}
+.linknav:hover {
+  color: lightslategray;
 }
 </style>
