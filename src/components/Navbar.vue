b1e1d4991f2c3e725857e21bdac6d5628ba84f2d
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-07-01 09:05:10 +0000

    Update 3 files

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,8 @@
 <template>
   <div class="navbar-menu">
     <ul>
-      <router-link to="/">Home</router-link>
+      <router-link class="linknav" to="/">Home</router-link>
+      <router-link class="linknav" to="/">Global Network</router-link>
     </ul>
   </div>
 </template>
@@ -11,6 +12,15 @@
 <style>
 .navbar-menu {
   padding: 25px;
-  background-color: white;
+  background-color: rgb(20, 26, 80);
+}
+.navbar-menu ul {
+  display: block;
+}
+.linknav {
+  color: white;
+  text-decoration: none;
+  font-family: sans-serif;
+  margin: 8px;
 }
 </style>
