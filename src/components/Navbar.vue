7c02355883aa97be6ca2394aa0345464b9e6d208
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-07-01 08:45:28 +0000

    Update 4 files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <div class="navbar-menu">
+    <ul>
+      <router-link to="/">Home</router-link>
+    </ul>
+  </div>
+</template>
+
+<style>
+.navbar-menu {
+  padding: 25px;
+  background-color: white;
+}
+</style>
