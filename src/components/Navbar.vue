d3df25afb9844d910eca8c0925c7ebeb1b1e2d2e
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-11-12 15:48:39 +0000

    becoming a cloud powered by teamlitho.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="navbar-menu">
     <ul>
-      <p class="title">Techtack Technologies</p>
+      <p class="title">Techtack-Technologies Cloud</p>
       <router-link class="linknav" to="/">Home</router-link>
       <router-link class="linknav" to="/about">About</router-link>
       <router-link class="linknav" to="/insiders/GlobalNetwork"
